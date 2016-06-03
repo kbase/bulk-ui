66eb88c466917c43f56ecb490bd09048bf08af45
nconrad <nealconrad@gmail.com>
2016-06-03 16:36:00 -0500

    minor things

--- app/job-details.view/job-details.view.ts
+++ app/job-details.view/job-details.view.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouteParams, RouteConfig, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
+import { MdProgressCircle } from '@angular2-material/progress-circle';
+
 import { Observable } from 'rxjs/Rx';
 
 import { JobService } from '../services/job.service';
@@ -11,6 +13,9 @@ import { Util } from '../services/util';
     styleUrls: ['app/job-details.view/job-details.view.html'],
     providers: [
         JobService
+    ],
+    directives: [
+        MdProgressCircle
     ]
 })
 
@@ -34,16 +39,20 @@ export class JobDetailsView implements OnInit {
 
     private loadStatus() {
         this.loading = true;
+
         // Fetch import jobs and filter out any jobs with non-leginimate-looking ids
         // next get individual job status
         // Note: a service would be very useful here.
-
         this.jobService.getJobInfo(this.id)
             .subscribe(jobInfo => {
                 let jobIds = jobInfo[12].split(',')
 
                 this.jobService.checkJobs(jobIds)
-                    .subscribe(jobs => this.jobs = jobs)
+                    .subscribe(jobs => {
+                        this.jobs = jobs;
+                        this.loading = false;
+                    })
+
             })
     }
 
