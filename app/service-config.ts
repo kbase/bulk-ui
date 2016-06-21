6e18d57a3aff01779fd937fc6994b952223ad134
nconrad <nealconrad@gmail.com>
2016-06-20 20:15:41 -0500

    use ci as default services

--- app/service-config.ts
+++ app/service-config.ts
@@ -1,8 +1,8 @@
 export const config = {
     endpoints: {
-        njs: "https://appdev.kbase.us/services/njs_wrapper",
-        ujs: "https://appdev.kbase.us/services/userandjobstate",
-        ws: "https://appdev.kbase.us/services/ws",                   // workspace service
+        njs: "https://ci.kbase.us/services/njs_wrapper",
+        ujs: "https://ci.kbase.us/services/userandjobstate",
+        ws: "https://ci.kbase.us/services/ws",                   // workspace service
         ftpApi: "http://kbase.us/services/kb-ftp-api/v0"
     },
     loginUrl: "https://narrative.kbase.us/#login",
